932e39f810e8c712ce8f63c368e01a877d5707b7
RohitKumarGit <59276987+RohitKumarGit@users.noreply.github.com>
2021-02-20 10:27:08 +0530

    update store index.js

--- src/store/index.js
+++ src/store/index.js
@@ -5,8 +5,50 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    selected:[],
+    total : 0,
+    shop:{},
+    email:""
   },
   mutations: {
+    store(context,shop){
+      context.shop = shop
+    },
+    email(context,em){
+      context.email = em
+    },
+    addp(context,item){
+      context.selected.push({id:item._id,count:1})
+      context.total+=item.price
+    },
+    removep(context,item){
+      
+      var m = 0
+      context.selected = context.selected.filter(v=>{
+        if(v.id == item._id){
+          m = v.count*item.price
+        }
+        return v.id!=item._id
+      })
+      console.log(m)
+      context.total = context.total  - m
+    },
+    incdec(context,data){
+      
+      const m = context.selected.map(v=>{
+          if(v.id == data.item._id){
+            context.total = context.total - v.count*data.item.price + data.count*data.item.price
+            return {
+              id:data.item._id,
+              count :data.count
+            }
+          }
+          else {
+            return v
+          }
+      })
+      context.selected = m
+    }
   },
   actions: {
   },
