2284bcdba261c8992268a99fce35adfe6d4f924e
Deepak prakash <deepakdpyqaz@gmail.com>
2021-02-20 09:41:21 +0530

    created product and topbar

---
+++ src/components/topbar.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="topbar mb-3">
+        <div class="right">
+         <div class="logo has-text-centered pt-2 is-size-5 has-text-weight-bold has-text-primary">PAYFAST</div>
+        </div>
+        <div class="left">
+        <b-button type="is-primary" class="mt-1 mb-1" v-on:click="logout">Log Out</b-button>
+        </div>
+    </div>
+</template>
+<script>
+import firebase from 'firebase'
+export default {
+    methods:{
+        logout(){
+            firebase.auth().signOut().then(() => {
+  // Sign-out successful.
+    window.location = '/'
+}).catch((error) => {
+  // An error happened.
+  console.log(error)
+})
+        }
+    }
+}
+</script>
